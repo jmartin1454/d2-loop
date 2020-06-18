3d67e86ac91ac10cc93a8d135845589f0ec5bb2c
Jeffery William Martin <j.martin@uwinnipeg.ca>
2020-06-18 10:38:06 -0500

    Updating for unknown reason (not the working code)

--- d2-loop.py
+++ d2-loop.py
@@ -37,3 +37,49 @@ for T in np.arange(18.624,25,0.01):
     d=CP.PropsSI('D','T',T,'Q',0,fluid)
     p=CP.PropsSI('P','T',T,'Q',0,fluid)
     print(T,d,p)
+
+# Based on Cam's design
+    
+p_psi=20. # PSI
+p=p_psi*6894.76 # Pa
+
+# What's the temperature at which the two-phase gas-liquid condition exists (saturated state)
+
+Tmax=CP.PropsSI('T','P',p,'Q',0,fluid)
+print(Tmax)
+
+# What's the temperature at which it solidifies?
+
+#p=101325
+
+state=CP.AbstractState("","Deuterium")
+print(state.has_melting_line())
+# Conclusion:  seems like CoolProp does not have this information.
+
+p_psi=20. # PSI
+p=p_psi*6894.76 # Pa
+T=23.0 # K
+
+h1=CP.PropsSI('H','T',T,'P',p,fluid)
+phase=CP.PhaseSI('T',T,'P',p,fluid)
+print(T,p,h1,phase)
+
+T=24.0 # K
+h2=CP.PropsSI('H','T',T,'P',p,fluid)
+density=CP.PropsSI('D','T',T,'P',p,fluid)
+phase=CP.PhaseSI('T',T,'P',p,fluid)
+print(T,p,h2,phase)
+
+qdot=50 # W
+delta_t=0.125*density*(h2-h1)/qdot
+print(delta_t/60)
+
+mdot=qdot/(h2-h1) # kg/s
+print(mdot)
+
+
+T=20.5 # K
+p_psi=20. # PSI
+p=p_psi*6894.76 # Pa
+viscosity=CP.PropsSI('VISCOSITY','T',T,'P',p,'hydrogen')
+print(viscosity)
